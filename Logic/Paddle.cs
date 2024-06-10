9c3fc540618c1be3479bfe656b38d398a8f65df7
anthony-gg <iacano5@outlook.com>
2024-06-10 11:59:31 -0400

    added ability to slow with keybind

--- Logic/Paddle.cs
+++ Logic/Paddle.cs
@@ -9,6 +9,7 @@ public partial class Paddle : Area2D
     private int _ballDir;
     private string _up;
     private string _down;
+    private string _slow;
 
     public override void _Ready()
     {
@@ -16,16 +17,24 @@ public partial class Paddle : Area2D
         _up = name + "_move_up";
         _down = name + "_move_down";
         _ballDir = name == "left" ? 1 : -1;
+        _slow = name + "_slow_speed";
     }
 
     public override void _Process(double delta)
     {
         // Move up and down based on input.
         float input = Input.GetActionStrength(_down) - Input.GetActionStrength(_up);
-        Vector2 position = Position; // Required so that we can modify position.y.
-        position += new Vector2(0, input * MoveSpeed * (float)delta);
-        position = new(position.X, Mathf.Clamp(position.Y, 16, GetViewportRect().Size.Y - 16));
-        Position = position;
+        if (Input.GetActionStrength(_slow) > 0){
+            Vector2 position = Position; // Required so that we can modify position.y.
+            position += new Vector2(0, input * (MoveSpeed/2) * (float)delta);
+            position = new(position.X, Mathf.Clamp(position.Y, 16, GetViewportRect().Size.Y - 16));
+            Position = position;
+        } else {
+            Vector2 position = Position; // Required so that we can modify position.y.
+            position += new Vector2(0, input * MoveSpeed * (float)delta);
+            position = new(position.X, Mathf.Clamp(position.Y, 16, GetViewportRect().Size.Y - 16));
+            Position = position;
+        }
     }
 
     public void OnAreaEntered(Area2D area)
